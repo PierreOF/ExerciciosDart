c318766d657c982b33bc4aa005b2488ffe4cb28f
PierreOF <marcos.pierre@upe.br>
2025-03-18 21:57:25 -0300

    primeiros exercicios

---
+++ exercicio01.dart
@@ -0,0 +1,9 @@
+
+void main() {
+  int numero = 7; 
+  print('Tabuada de $numero:');
+  
+  for (int i = 1; i <= 10; i++) {
+    print('$numero x $i = ${numero * i}');
+  }
+}
---
+++ exercicio02.dart
@@ -0,0 +1,6 @@
+void main() {
+  int porcentagem_desconto = 10;
+  double preco_produto = 100;
+
+  print(preco_produto - ((porcentagem_desconto / 100) * preco_produto));
+}
